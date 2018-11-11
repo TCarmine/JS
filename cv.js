c987aae2d9daaa2ab6320cdf81ff5a785346195e
Carmine <kappa85t@hotmail.it>
2018-11-11 14:09:29 +0100

    added js file, grab html elements

---
+++ cv.js
@@ -0,0 +1,11 @@
+// string methods
+
+
+// 1. lastIndexOF
+// 2. substring
+
+let form = document.querySelector('form');
+let inputs = document.querySelectorAll('fieldset > input');
+let tArea = document.querySelector('textarea');
+let liInputs = document.querySelectorAll('li  > input');
+let backgroundColor = document.querySelector('#primaryColor');
