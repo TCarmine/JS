23e95070888c9a515d70b07f9dbfa8d2e813c7eb
Carmine <kappa85t@hotmail.it>
2018-11-11 14:13:18 +0100

    create section and h3 element and appended to the section

--- cv.js
+++ cv.js
@@ -9,3 +9,19 @@ let inputs = document.querySelectorAll('fieldset > input');
 let tArea = document.querySelector('textarea');
 let liInputs = document.querySelectorAll('li  > input');
 let backgroundColor = document.querySelector('#primaryColor');
+
+
+form.addEventListener('submit', function(ev){
+
+  ev.preventDefault();
+  let section = document.createElement('SECTION');
+  let name = inputs[0].value;
+  let h3 = document.createElement('H3');
+
+  h3.style.marginBottom = '2vw';
+  h3.innerText=name;
+  section.appendChild(h3);
+
+  document.body.appendChild(section);
+
+})
