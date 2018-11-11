37b6e051501120fe275fee22f216507016b8c60e
Carmine <kappa85t@hotmail.it>
2018-11-11 14:22:48 +0100

    created date element and filled with the date picked

--- cv.js
+++ cv.js
@@ -13,14 +13,21 @@ let backgroundColor = document.querySelector('#primaryColor');
 
 form.addEventListener('submit', function(ev){
 
+  // preventDefault is necessary for submit event type
   ev.preventDefault();
   let section = document.createElement('SECTION');
+
   let name = inputs[0].value;
   let h3 = document.createElement('H3');
 
   h3.style.marginBottom = '2vw';
   h3.innerText=name;
   section.appendChild(h3);
+
+  let date = inputs[1].value;
+  let p = document.createElement('P');
+  p.innerText ='Born at ' + date;
+  section.appendChild(p);
 
   document.body.appendChild(section);
 
