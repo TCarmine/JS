616555c8a8daaca1e896dbdff980762e04588fe3
Carmine <kappa85t@hotmail.it>
2018-11-26 14:21:40 +0100

    refactoring with time variable

--- Callbacks_Promisis/promises.js
+++ Callbacks_Promisis/promises.js
@@ -1,38 +1,40 @@
 let givenName = 'Cersel';
 
-let asynChangeName = newName => {
+let asynChangeName = ( newName , time ) => {
   let promise = new Promise((resolve, recject) => {
     setTimeout(() => {
       resolve(newName);
-    }, 5000);
+    }, time);
   })
   return promise;
 }
 //let names = ['Euron Greyjoy', 'Jamie Lanniester','Night Kings'];
-asynChangeName('Danny')
+asynChangeName('Danny', 5000)
   .then( name => {
     givenName = name;
     console.log(givenName);
-    return asynChangeName('Jon Snow');
+    return asynChangeName('Jon Snow', 4000);
   })
   .then( name =>{
     givenName = name;
     console.log(givenName);
-    return asynChangeName('Euron Greyjoy');
+    return asynChangeName('Euron Greyjoy', 3000);
   })
   .then( name =>{
     givenName = name;
     console.log(givenName);
-    return asynChangeName('Jamie Lanniester');
+    return asynChangeName('Jamie Lanniester', 2000);
 
   })
   .then( name =>{
     givenName = name;
     console.log(givenName);
-    return asynChangeName('Night Kings');
+    return asynChangeName('Night Kings', 1000);
 
   })
   .then( name =>{
     givenName = name;
     console.log(givenName);
   })
+
+  console.log(givenName);
