ddb472cf15b803fb25c23797ed1a5f169c3d924d
Carmine <kappa85t@hotmail.it>
2018-11-26 14:39:49 +0100

    refactor with counter

--- Callbacks_Promisis/promises.js
+++ Callbacks_Promisis/promises.js
@@ -3,38 +3,54 @@ let givenName = 'Cersel';
 let asynChangeName = ( newName , time ) => {
   let promise = new Promise((resolve, recject) => {
     setTimeout(() => {
-      resolve(newName);
+      resolve(newName, time);
     }, time);
   })
   return promise;
 }
-//let names = ['Euron Greyjoy', 'Jamie Lanniester','Night Kings'];
-asynChangeName('Danny', 5000)
-  .then( name => {
-    givenName = name;
-    console.log(givenName);
-    return asynChangeName('Jon Snow', 4000);
-  })
-  .then( name =>{
-    givenName = name;
-    console.log(givenName);
-    return asynChangeName('Euron Greyjoy', 3000);
-  })
-  .then( name =>{
-    givenName = name;
-    console.log(givenName);
-    return asynChangeName('Jamie Lanniester', 2000);
+let names = ['Euron Greyjoy', 'Jamie Lanniester','Night Kings'];
+let counter = 0;
 
-  })
-  .then( name =>{
-    givenName = name;
-    console.log(givenName);
-    return asynChangeName('Night Kings', 1000);
+let makeNewNames = counter => {
+  if (counter === names.length){
+    return;
+  }
+  asynChangeName(names[counter], 3000)
+      .then( name => {
+        givenName = name;
+        console.log(givenName);
+        counter++;
+        makeNewNames(counter);
+      })
+}
 
-  })
-  .then( name =>{
-    givenName = name;
-    console.log(givenName);
-  })
+makeNewNames(counter);
+// asynChangeName('Danny', 5000)
+//   .then( name => {
+//     givenName = name;
+//     console.log(givenName);
+//     return asynChangeName('Jon Snow', 4000);
+//   })
+//   .then( name =>{
+//     givenName = name;
+//     console.log(givenName);
+//     return asynChangeName('Euron Greyjoy', 3000);
+//   })
+//   .then( name =>{
+//     givenName = name;
+//     console.log(givenName);
+//     return asynChangeName('Jamie Lanniester', 2000);
+//
+//   })
+//   .then( name =>{
+//     givenName = name;
+//     console.log(givenName);
+//     return asynChangeName('Night Kings', 1000);
+//
+//   })
+//   .then( name =>{
+//     givenName = name;
+//     console.log(givenName);
+//   })
 
   console.log(givenName);
