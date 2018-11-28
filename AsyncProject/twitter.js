c68e28dbfa69536e002fc7b913ffc0211779920a
Carmine <kappa85t@hotmail.it>
2018-11-28 18:05:59 +0100

    callback outside eventlistner

--- AsyncProject/twitter.js
+++ AsyncProject/twitter.js
@@ -1,33 +1,37 @@
 let lis = document.querySelectorAll('li');
+let container = document.querySelector('#container');
 
 
-lis[1].addEventListener('click',(ev)=>{
+let grap = async dataJson =>{
   let data = 'data.json';
-  let grap = async dataJson =>{
+  let res = await fetch(dataJson);
+  let finalData = await res.json();
+  console.log(finalData);
+  let arrayDivs =[];
 
-    let res = await fetch(dataJson);
-    let finalData = await res.json();
-    console.log(finalData);
-    let arrayDivs =[];
-    for(let i=0; i<finalData.length;i++){
-        let div = document.createElement('div');
-        let image = document.createElement('img');
-        let h3 = document.createElement('h3');
-        let p = document.createElement('p');
-        image.src = finalData[i].address;
-        h3.innerText = finalData[i].title;
-        p.innerText = finalData[i].description;
-        div.appendChild(image);
-        div.appendChild(h3);
-        div.appendChild(p);
-        document.body.appendChild(div);
+  for(let i=0; i<finalData.length;i++){
+      let section = document.createElement('section');
+      let image = document.createElement('img');
+      let h3 = document.createElement('h3');
+      let p = document.createElement('p');
+      let imgStringPath = finalData[i].address;
+      image.src = imgStringPath;
+      h3.innerText = finalData[i].title;
+      p.innerText = finalData[i].description;
 
-
-
-    }
-
-
-
+      section.appendChild(image);
+      section.appendChild(h3);
+      section.appendChild(p);
+      container.appendChild(section);
   }
-  grap(data);
-})
+
+}
+
+// grap(data);
+
+lis[1].addEventListener('click',grap);
+
+
+// img.addEventListener('click', ev =>){
+//
+// }
