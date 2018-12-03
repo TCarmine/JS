f9e192ff86e1fdefc89cc0f6e311f073cfd14e8e
Carmine <kappa85t@hotmail.it>
2018-12-03 16:11:15 +0100

    some refactoring to async

--- AsyncProject/AsyncAwait/async.js
+++ AsyncProject/AsyncAwait/async.js
@@ -2,48 +2,7 @@
 let exampleUrl = 'https://reqres.in/api/users';
 let btn = document.querySelector('button');
 
-let getUsers = ev => {
-  fetch(exampleUrl)
-    .then(response => response.json())
-    .then(obj => {
-      console.log(obj.data);
-      for (let user of obj.data) {
-        let section = document.createElement('SECTION');
-        let firstHeading = document.createElement('H3');
-        let secondHeading = document.createElement('H3');
-        let image = document.createElement('IMG');
-        firstHeading.innerText = user.first_name;
-        secondHeading.innerText = user.last_name;
-        image.src = user.avatar;
-        section.appendChild(firstHeading);
-        section.appendChild(secondHeading);
-        section.appendChild(image);
-        document.body.appendChild(section);
-      }
-    })
-}
 
-let commentUrl = 'http://35.156.88.18:3050/comments';
-
-let bringComments = ev => {
-  fetch(commentUrl)
-    .then(res => res.json())
-    .then(commentsInfo => {
-      for (let commentInfo of commentsInfo) {
-        let section = document.createElement('SECTION');
-        let headingName = document.createElement('H3');
-        let comment = document.createElement('P');
-        headingName.innerText = commentInfo.name;
-        comment.innerText = commentInfo.body;
-        section.appendChild(headingName);
-        section.appendChild(comment);
-        document.body.appendChild(section);
-      }
-    })
-    .catch(err => {
-      console.log(err);
-    })
-}
 const authToken = '16d4785f9c10724266053adb3c29dcfd';
 
 let inputField = document.querySelector('input');
