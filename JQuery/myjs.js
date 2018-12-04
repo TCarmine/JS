e8ba5c46d72bdb2399d4912365f09d9d4d647416
Carmine <kappa85t@hotmail.it>
2018-12-04 23:35:26 +0100

    some basic Jquery

---
+++ JQuery/myjs.js
@@ -0,0 +1,13 @@
+  // let btn = document.querySelector('button');
+
+$().ready(ev=>{
+
+        $('button').click(ev=>{
+        // btn.click(ev=>{
+
+          // let p = document.querySelector('p');
+          //  p.hide();
+
+          $('p').fadeToggle(1500); //show(), toggle()
+        })
+})
