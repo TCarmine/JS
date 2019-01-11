d53ff32b40c2e0348802c5e0bba94d9562ec98e6
Carmine <kappa85t@hotmail.it>
2019-01-11 11:48:28 +0100

    basic button event click

--- AsyncProject/Promises/basic.js
+++ AsyncProject/Promises/basic.js
@@ -1,5 +1,13 @@
-let logCall =  () => {
-  console.log('logCall was called');
-};
+let btn = document.querySelector('button');
 
-setTimeout(logCall, 10000);
+btn.addEventListener("click", function(e){
+  console.log("The button was clicked");
+})
+
+// basic callbacks with setTimeout
+
+// let logCall =  () => {
+//   console.log('logCall was called');
+// };
+//
+// setTimeout(logCall, 10000);
