e1383b2b73dc73a1d8f9386da8bc4ba18d2ec796
Carmine <kappa85t@hotmail.it>
2019-01-13 19:49:09 +0100

    added jquery loader and css loader

--- webpack_app/app.js
+++ webpack_app/app.js
@@ -1,4 +1,12 @@
-let people= require('./people.js');
+require('!style-loader!css-loader!./style.css');
 
+let people= require('./people.js');
+let $ = require('jquery');
+
+
+
+$.each(people, function(key, value){
+  $('body').append('<h1>'+people[key].name+'<h1>');
+})
 
 console.log(people[2].name);
